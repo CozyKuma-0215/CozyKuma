d34e0e88792d8013f930703b740cbc98c90a8ac6
CozyKuma-0215 <cozykumastudio@gmail.com>
2025-04-14 19:45:02 +0900

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,17 +10,13 @@ const FILES_TO_CACHE = [
   "./offline.html"
 ];
 
-// 설치 단계
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// 활성화 단계 (이전 캐시 삭제)
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keyList => {
@@ -36,15 +32,10 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-// 요청 가로채기
 self.addEventListener("fetch", event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(response => response || fetch(event.request))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match(OFFLINE_URL));
+    })
+  );
 });
